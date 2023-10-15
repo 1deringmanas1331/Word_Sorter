bf21692200f89dacb584e7d0b983e5ffd2f56f8b
Manas R <122677792+1deringmanas1331@users.noreply.github.com>
2023-10-15 15:35:32 +0530

    Add files via upload

---
+++ Word_Sorter.py
@@ -0,0 +1,16 @@
+
+names = []
+
+
+while True:
+    name = input("Enter a name (or press Enter to finish): ")
+    if name == "":
+        break
+    names.append(name)
+
+sorted_names = sorted(names)
+
+
+print("Sorted Names in Alphabetical Order:")
+for name in sorted_names:
+    print(name)
