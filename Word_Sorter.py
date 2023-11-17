af34b972337ea81691943167dc4cb74596c25f21
Manas R <122677792+1deringmanas1331@users.noreply.github.com>
2023-11-17 06:23:15 +0530

    Update Word_Sorter.py

--- Word_Sorter.py
+++ Word_Sorter.py
@@ -1,6 +1,7 @@
 #Add the names which needs to be sorted
 names = []
 while True:
+    # Asks the user for the input name
     name = input("Enter a name (or press Enter to finish): ")
     if name == "":
         break
