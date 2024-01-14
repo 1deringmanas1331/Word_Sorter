04a5975d82c9a139aa6a4aff57a68486f2152389
Manas R <122677792+1deringmanas1331@users.noreply.github.com>
2024-01-14 07:21:13 +0530

    Word_Sorter.py

--- Word_Sorter.py
+++ Word_Sorter.py
@@ -1,4 +1,4 @@
-# Add the names into the list which needs to be sorted
+# Add the Names into the list which needs to be sorted
 names = []
 while True:
     # Asks the user for the input names and adds it to the list
